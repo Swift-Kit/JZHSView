9649c44fb0fba211a83103c02780e96602b6f87e
Joey Zhou <josephyzhou@gmail.com>
2013-12-18 11:48:11 -0800

    It’s shaking!

--- JZHSView/JZHSView/JZHSViewController.m
+++ JZHSView/JZHSView/JZHSViewController.m
@@ -8,6 +8,7 @@
 
 #import "JZHSViewController.h"
 @interface JZHSViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
 
 @end
 
@@ -23,6 +24,7 @@
     myAudioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:fileURL error:nil];
     myAudioPlayer.numberOfLoops = 1;
     [self startMusic:self];
+    [self shakeLabel:self.titleLabel];
 }
 
 - (void)didReceiveMemoryWarning
@@ -34,13 +36,53 @@
 - (IBAction)pauseMusic
 {
     NSLog(@"Pause music");
-
-        [myAudioPlayer stop];
+    [myAudioPlayer stop];
 }
 
 - (IBAction)startMusic:(id)sender
 {
     NSLog(@"Start music");
     [myAudioPlayer play];
+}
+
+- (void) shakeLabel: (UILabel *)labelToShake
+{
+    CGFloat t = 2.0;
+    CGAffineTransform translateRight  = CGAffineTransformTranslate(CGAffineTransformIdentity, t, 0.0);
+    CGAffineTransform translateLeft = CGAffineTransformTranslate(CGAffineTransformIdentity, -t, 0.0);
+    
+    labelToShake.transform = translateLeft;
+    
+    [UIView animateWithDuration:0.5x delay:0.0 options:UIViewAnimationOptionAutoreverse|UIViewAnimationOptionRepeat animations:^{
+        [UIView setAnimationRepeatCount:20.0];
+        labelToShake.transform = translateRight;
+    } completion:^(BOOL finished) {
+        if (finished) {
+            [UIView animateWithDuration:0.05 delay:0.0 options:UIViewAnimationOptionBeginFromCurrentState animations:^{
+                labelToShake.transform = CGAffineTransformIdentity;
+            } completion:NULL];
+        }
+    }];
+
+}
+
+- (void)shakeView:(UIView *)viewToShake
+{
+    CGFloat t = 2.0;
+    CGAffineTransform translateRight  = CGAffineTransformTranslate(CGAffineTransformIdentity, t, 0.0);
+    CGAffineTransform translateLeft = CGAffineTransformTranslate(CGAffineTransformIdentity, -t, 0.0);
+    
+    viewToShake.transform = translateLeft;
+    
+    [UIView animateWithDuration:0.07 delay:0.0 options:UIViewAnimationOptionAutoreverse|UIViewAnimationOptionRepeat animations:^{
+        [UIView setAnimationRepeatCount:2.0];
+        viewToShake.transform = translateRight;
+    } completion:^(BOOL finished) {
+        if (finished) {
+            [UIView animateWithDuration:0.05 delay:0.0 options:UIViewAnimationOptionBeginFromCurrentState animations:^{
+                viewToShake.transform = CGAffineTransformIdentity;
+            } completion:NULL];
+        }
+    }];
 }
 @end
